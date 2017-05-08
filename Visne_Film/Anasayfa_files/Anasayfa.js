b7909d189c10c175838111ed4c7c5e3bab68e39a
visnefilm <gokhan@Gokhans-Mac-Pro.local>
2017-05-08 23:53:01 +0300

    Update

--- Visne_Film/Anasayfa_files/Anasayfa.js
+++ Visne_Film/Anasayfa_files/Anasayfa.js
@@ -1,11 +1,9 @@
 // Created by iWeb 3.0.4 local-build-20170508
 
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Anasayfa_files/AnasayfaMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
