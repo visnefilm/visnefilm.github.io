010a1562f1fe1ab615a9025fb9838c7e094098dc
visnefilm <gokhan@Gokhans-Mac-Pro.local>
2017-05-20 22:06:07 +0300

    All New

--- Visne_Film/Anasayfa_files/Anasayfa.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20170508
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Anasayfa_files/AnasayfaMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
